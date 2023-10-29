7c168adbf2481802209c7572e6a5464b4f5cef95
Daniel Anugerah <danielanugerah2004@gmail.com>
2023-10-29 23:54:49 +0800

    pass 3st stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,12 +12,30 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => _stream
-                .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                .expect("failed to write"),
+            Ok(mut _stream) => {
+                handle_connection(_stream);
+            }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let chunks: Vec<&str> = request_line.split_whitespace().collect();
+
+    let path = chunks[1];
+
+    if path == "/" {
+        stream
+            .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+            .expect("Unable to write to stream");
+    } else {
+        stream
+            .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+            .expect("Unable to write to stream");
     }
 }
