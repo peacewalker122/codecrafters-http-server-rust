b318fd4191a4ffe4ed3c301602595a3dad88b679
Daniel Anugerah <danielanugerah2004@gmail.com>
2023-10-30 21:30:10 +0800

    retry 4st stage

--- src/main.rs
+++ src/main.rs
@@ -29,31 +29,27 @@ fn handle_connection(mut stream: TcpStream) {
 
     let path = chunks[1]; //ex: /echo/abc
 
-    let pathwithdata: Vec<&str> = path.split("/").filter(|x| x.len() > 0).collect();
+    let pathwithdata: Vec<&str> = path.splitn(3, "/").filter(|x| x.len() > 0).collect();
 
-    // dbg!(path);
-    // dbg!(pathwithdata);
+    dbg!(&path);
+    dbg!(&pathwithdata);
 
     let containecho = path.contains("echo");
 
     match containecho {
         true => {
-            if let Some(x) = pathwithdata.get(0) {
-                if *x == "echo" {
-                    let contentlength = format!("Content-length: {}\r\n", pathwithdata[1].len());
-                    let msg = vec![
-                        "HTTP/1.1 200 OK\r\n",
-                        "Content-Type: text/plain\r\n",
-                        &contentlength,
-                        "\r\n",
-                        pathwithdata[1],
-                    ];
+            let contentlength = format!("Content-length: {}\r\n", pathwithdata[1].len());
+            let msg = vec![
+                "HTTP/1.1 200 OK\r\n",
+                "Content-Type: text/plain\r\n",
+                &contentlength,
+                "\r\n",
+                pathwithdata[1],
+            ];
 
-                    stream
-                        .write_all(handle_content(&msg).as_bytes())
-                        .expect("Unable to write to stream");
-                }
-            }
+            stream
+                .write_all(handle_content(&msg).as_bytes())
+                .expect("Unable to write to stream");
         }
         false => {
             match path {
